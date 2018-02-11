4355a2137051e564fd5b501a66f9bfb94fcc7c61
池田弘樹 <ikedahiroki@ikedahirokinoMacBook-puro.local>
2018-02-11 18:57:49 +0900

    delete argument to main function

---
+++ source/hello.cpp
@@ -0,0 +1,14 @@
+///////////////////////////////////
+//                               //
+//     The first time GitHub     //
+//                               //
+///////////////////////////////////
+
+#include<iostream>
+
+int main( void ){
+
+    std::cout << "Hello GitHub!" << std::endl;
+
+    return 0;
+}
